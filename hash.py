4bbf22297cfcc8b920fcb3a533fab62753122e10
S Hari Priya <haripriyasri1006@gmail.com>
2026-04-02 09:09:08 +0530

    Delete hash.py

--- hash.py
+++
@@ -1,9 +0,0 @@
-def polynomial_hash(message, base=31, mod=10**9 + 7):
-    hash_value = 0
-    power = 1
-
-    for char in message:
-        hash_value = (hash_value + ord(char) * power) % mod
-        power = (power * base) % mod
-
-    return hash_value
